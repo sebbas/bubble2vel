3ed6a0ac143610b240b0094698b49c198ccb063c
Sebastián Barschkis <sebbas@sebbas.org>
2022-08-21 13:56:16 -0700

    Main: Removed prediction and figure plotting calls

--- src/bmain.py
+++ src/bmain.py
@@ -63,13 +63,11 @@ parser.add_argument('-bs', '--batchSize', type=int, default=128,
 parser.add_argument('-restart', '--restart', default=False, action='store_true',
                     help='restart from checkpoint')
 parser.add_argument('-ckpnt', '--checkpoint', default=None, help='checkpoint name')
-parser.add_argument('-p', '--predictionFrames', type=int, default=1,
-                    help='predict all frames from start frame up to this frame')
 
 # Learning rate
 parser.add_argument('-lr0', '--lr0', type=float, default=5e-4, help='init learning rate')
 parser.add_argument('-lrmin', '--lrmin', type=float, default=1e-7, help='min learning rate')
-parser.add_argument('-pa', '--patience', type=int, default=200,
+parser.add_argument('-p', '--patience', type=int, default=200,
                     help='patience for reducing learning rate')
 parser.add_argument('-lr',  '--restartLr', type=float, default=None,
                      help='learning rate to restart training')
@@ -79,12 +77,6 @@ parser.add_argument('-g', '--saveGradStat', default=False, action='store_true',
                     help='save gradient statistics')
 parser.add_argument('-ci', '--cachedInput', default=False, action='store_true',
                     help='extract boundary values from cached numpy arrays')
-                    
-# Plotting options
-parser.add_argument('-pd', '--plotDomain', default=False, action='store_true',
-                    help='plot prediction velocities for the entire domain')
-parser.add_argument('-ev', '--exportVideo', default=False, action='store_true',
-                    help='export image data to video (using ffmpeg)')
 
 args = parser.parse_args()
 
@@ -118,7 +110,6 @@ print('{} samples in training, {} in validation'.format(nTrain, nValid))
 assert nValid > 0, 'Number of validation samples must be greater than 0'
 
 normalizeXyt = True
-onlyFluid = not args.plotDomain
 
 ### Generators
 trainGen = dataSet.generate_trainval_pts(0, nTrain, normalizeXyt=normalizeXyt, batchSize=args.batchSize)
@@ -164,139 +155,5 @@ bubbleNet.fit(
 losses = pd.DataFrame(bubbleNet.history.history)
 fig = losses.plot().get_figure()
 Util.save_figure_dataframe(losses, fig, 'stats', 'losses')
-
-### Prediction
-
-assert args.predictionFrames <= args.totalFrames, 'Cannot use more prediction frames than total frames'
-assert args.predictionFrames > 0, 'Must use at least 1 prediction frame'
-
-size    = dataSet.get_size()
-
-predStart, predEnd = 0, args.predictionFrames
-predVels = None
-predGen = dataSet.generate_predict_pts(predStart, predEnd, onlyFluid=onlyFluid)
-predVels = bubbleNet.predict(predGen)
-
-nExpectedPred = dataSet.get_num_fluid(0, args.predictionFrames) if onlyFluid else dataSet.get_num_cells() * args.predictionFrames
-assert len(predVels) == nExpectedPred, 'Prediction cell count mismatch, expected {} vs {}'.format(nExpectedPred, len(predVels))
-
-### Plots
-
-predVelOffset = 0 # just a helper var to find beginning of next frame in predVels array
-for frame in range(predStart, predEnd):
-
-  xyBc    = dataSet.get_xy_bc(frame)
-  xyFluid = dataSet.get_xy_fluid(frame)
-  bc      = dataSet.get_bc(frame)
-
-  ### Original vel plots
-
-  arrow_res = 2
-  x = np.arange(0, int(size[0]), arrow_res)
-  y = np.arange(0, int(size[1]), arrow_res)
-  X, Y = np.meshgrid(x, y, indexing='xy')
-
-  origvelGrid = np.zeros((size[0], size[1], nDim))
-  origvelMag = np.zeros((size))
-  for pos, vel in zip(xyBc, bc):
-    x, y = int(pos[0]), int(pos[1])
-    origvelMag[x, y] = np.sqrt(vel[0]**2 + vel[1]**2)
-    origvelGrid[x, y, :] = vel
-
-  if Util.PRINT_DEBUG:
-    print('origvel max u {}, max v {}'.format(max( origvelGrid[:,:,0].min(), origvelGrid[:,:,0].max(), key=abs ),
-                                              max( origvelGrid[:,:,1].min(), origvelGrid[:,:,1].max(), key=abs )))
-  Util.save_image(src=origvelMag, subdir='origvels', name='origvels_pts', frame=frame)
-
-  velsU = origvelGrid[::arrow_res,::arrow_res,0]
-  velsV = origvelGrid[::arrow_res,::arrow_res,1]
-  origvelMag = origvelMag[::arrow_res,::arrow_res]
-
-  mask = origvelMag > 0
-  velsU = velsU[mask]
-  velsV = velsV[mask]
-  X = X[mask]
-  Y = Y[mask]
-  velsMag = origvelMag[mask]
-
-  fig, ax = plt.subplots(1, 1, figsize=(10,10))
-  plot = ax.quiver(X, Y, velsU, velsV, velsMag, cmap='winter', scale_units = 'xy', angles='xy')
-  ax.set_xlim(0, size[0])
-  ax.set_ylim(0, size[1])
-  ax.invert_yaxis()
-  Util.save_figure_plot(fig, 'origvels', 'origvels', frame, colorbar=True, plot=plot, cmin=0, cmax=0.8)
-
-  ### Prediction vel plots
-
-  if predVels is not None:
-    arrow_res = 16
-    x = np.arange(0, int(size[0]), arrow_res)
-    y = np.arange(0, int(size[1]), arrow_res)
-    X, Y = np.meshgrid(x, y, indexing='xy')
-
-    predvelGrid = np.zeros((size[0], size[1], nDim))
-    predvelMag = np.zeros((size))
-
-    gridXy = [(x,y,frame) for x in range(size[0]) for y in range(size[1])]
-    loopArray = xyFluid if onlyFluid else gridXy
-    cnt = 0
-    for xi, yi, _ in loopArray:
-      xi, yi = int(xi), int(yi)
-      vel = predVels[cnt + predVelOffset, :]
-      predvelMag[xi, yi] = np.sqrt(vel[0]**2 + vel[1]**2)
-      predvelGrid[xi, yi, :] = vel
-      cnt += 1
-
-    if onlyFluid:
-      nExpectedFluid = dataSet.get_num_fluid(frame, frame+1)
-      assert cnt == nExpectedFluid, 'Fluid cell count mismatch, expected {} vs {}'.format(nExpectedFluid, cnt)
-
-    # Start of the predvels of next frame
-    predVelOffset += cnt
-
-    if Util.PRINT_DEBUG:
-      print('predvel max u {}, max v {}'.format(max( predvelGrid[:,:,0].min(), predvelGrid[:,:,0].max(), key=abs ),
-                                                max( predvelGrid[:,:,1].min(), predvelGrid[:,:,1].max(), key=abs )))
-    Util.save_image(src=predvelMag, subdir='predvels', name='predvels_pts', frame=frame)
-
-    predVelsU = predvelGrid[::arrow_res, ::arrow_res, 0]
-    predVelsV = predvelGrid[::arrow_res, ::arrow_res, 1]
-    predvelMag = predvelMag[::arrow_res, ::arrow_res]
-
-    mask = predvelMag > 0.0
-
-    predVelsU = predVelsU[mask]
-    predVelsV = predVelsV[mask]
-    X = X[mask]
-    Y = Y[mask]
-    predVelsMag = predvelMag[mask]
-
-    fig, ax = plt.subplots(1, 1, figsize=(10,10))
-    plot = ax.quiver(X, Y, predVelsU, predVelsV, predVelsMag, cmap='winter', scale_units = 'xy', angles='xy')
-    ax.set_xlim(0, size[0])
-    ax.set_ylim(0, size[1])
-    ax.invert_yaxis()
-    Util.save_figure_plot(fig, 'predvels', 'predvels', frame, colorbar=True, plot=plot, cmin=0, cmax=0.4)
-
-    # Plot with matplotlib streamplot
-    fig, ax = plt.subplots(1, 1, figsize=(10,10))
-    x = np.arange(0, int(size[0]), 1)
-    y = np.arange(0, int(size[1]), 1)
-    X, Y = np.meshgrid(x, y, indexing='xy')
-    predVelsU = predvelGrid[:,:,0]
-    predVelsV = predvelGrid[:,:,1]
-    plot = ax.streamplot(X, Y, predVelsU, predVelsV, density=2.5, linewidth=1.0)#, color='#A23BEC')
-    ax.set_xlim(0, size[0])
-    ax.set_ylim(0, size[1])
-    ax.invert_yaxis()
-    Util.save_figure_plot(fig, 'predvels', 'predvels_stream', frame)
-
-# Optional video export with ffmpeg
-if args.exportVideo:
-  Util.save_video(subdir='origvels', name='origvels', imgDir='../img/origvels/', fps=15)
-  Util.save_video(subdir='predvels', name='predvels', imgDir='../img/predvels/', fps=15)
-  Util.save_video(subdir='origvels', name='origvels_pts', imgDir='../img/origvels/', fps=15)
-  Util.save_video(subdir='predvels', name='predvels_pts', imgDir='../img/predvels/', fps=15)
-  Util.save_video(subdir='predvels', name='predvels_stream', imgDir='../img/predvels/', fps=15)
 
 

d3a49caad83702cf8d23e94b1129bb8d65abe782
Sebastián Barschkis <sebbas@sebbas.org>
2022-10-31 10:21:40 -0700

    Layers: Use tanh activation instead of swish everywhere

--- src/bmodel.py
+++ src/bmodel.py
@@ -24,9 +24,9 @@ class BubblePINN(keras.Model):
     self.width = width
     self.reg   = reg
     if reg == None:
-      self.mlp = DenseLayers(width=width, prefix='bc', last_linear=True)
+      self.mlp = DenseLayers(width=width, prefix='bc', act='tanh', last_linear=True)
     else:
-      self.mlp = DenseLayers(width=width, reg=reg, prefix='bc', last_linear=True)
+      self.mlp = DenseLayers(width=width, reg=reg, prefix='bc', act='tanh', last_linear=True)
     # Coefficient for data and pde loss
     self.alpha = alpha
     self.beta  = beta

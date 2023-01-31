953bee661fe468cbb90daa078362536e9859e7f0
Sebastián Barschkis <sebbas@sebbas.org>
2023-01-31 14:21:21 -0800

    Generator: Added options for simulation files
    The source type can now be specified (flownet experimental data or flashx simulation data)

--- src/bgenerate.py
+++ src/bgenerate.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 import bdataset as BD
-import butils as Util
+import butils as UT
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', '--file', default='../data/PB_30W_RGB/512/30W%06d.flo',
-                    help='the file(s) containing flow velocity training data')
 parser.add_argument('-s', '--startFrame', type=int, default=0,
                     help='first frame to load from dataset')
 parser.add_argument('-e', '--endFrame', type=int, default=399,
@@ -16,21 +15,34 @@ parser.add_argument('-w', '--walls', type=int, nargs=4, default=[1,1,1,1],
                     help='domain walls, [top, right, bottom, left]')
 parser.add_argument('-i', '--interface', type=int, default=1,
                     help='thickness of bubble liquid interface in pixels')
+parser.add_argument('-src', '--source', type=int, default=0,
+                    help='type of training data source, either 1: experiment or 2: simulation')
 
 args = parser.parse_args()
 
-assert args.file.endswith('.flo')
+assert args.startFrame <= args.endFrame, 'Start frame must be smaller/equal than/to end frame'
+
+# Set source file location based on source type
+fName = None
+if args.source == UT.SRC_FLOWNET:
+  fName = '../data/PB_30W_RGB/512/30W%06d.flo'
+elif args.source == UT.SRC_FLASHX:
+  fName = '../data/PB_simulation/384/INS_Pool_Boiling_hdf5_plt_cnt_%04d'
+
+assert fName is not None, 'Invalid training data source'
 
 # Create dataset object,
-dataSet = BD.BubbleDataSet(fName=args.file, startFrame=args.startFrame, \
+dataSet = BD.BubbleDataSet(fName=fName, startFrame=args.startFrame, \
                            endFrame=args.endFrame, walls=args.walls, \
-                           interface=args.interface)
+                           interface=args.interface, source=args.source)
 
-# Extract points from images and save in arrays from dataset
-if not dataSet.load_data():
-  sys.exit()
+# Load training / validation data from files
+isLoaded, filePrefix = dataSet.load_data(args.source)
+if not isLoaded: sys.exit()
+
+# Extract points from dataset and store in h5
 dataSet.extract_wall_points()
 dataSet.extract_fluid_points(velEps=1.0)
-dataSet.save(walls=args.walls)
+dataSet.save(filePrefix=filePrefix)
 dataSet.summary()
 

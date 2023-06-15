844d2889fb92e8d1de1bddefc3291d8e8f72ea83
Sebastián Barschkis <sebbas@sebbas.org>
2023-06-15 13:48:29 -0700

    Dataset: Fix typo

--- src/bdataset.py
+++ src/bdataset.py
@@ -142,7 +142,7 @@ class BubbleDataSet:
             bcnt += 1
 
       # Filter out the outlier velocities above a certain velocity magnitude
-      filter = 0:
+      filter = 0
       if filter:
         velMag = np.sqrt(np.square(self.vel[:,:,:,0]) + np.square(self.vel[:,:,:,1]))
 

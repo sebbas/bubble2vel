a76189b2b49361206682758fc495341f5649e552
Sebastián Barschkis <sebbas@sebbas.org>
2023-06-21 17:10:40 -0700

    Model: Revert to lambda updates per batch

--- src/bmodel.py
+++ src/bmodel.py
@@ -366,33 +366,31 @@ class BModel(keras.Model):
         #                        lambda: 0.,
         #                        lambda: self.alpha))
 
-        if tf.equal(tf.math.mod(self.call_count, self.iter0), 0):
-          tf.print('Updating lambdas')
-          alpha = self.getAlpha()
-          rho = self.getRho()
+        alpha = self.getAlpha()
+        rho = self.getRho()
 
-          # compute new lambdas w.r.t. the losses in the previous iteration
-          lambdas_hat = [losses[i] / (self.last_losses[i] * self.temperature + EPS) for i in range(len(losses))]
-          lambdas_hat = tf.nn.softmax(lambdas_hat - tf.reduce_max(lambdas_hat)) * tf.cast(len(losses), dtype=tf.float32)
+        # compute new lambdas w.r.t. the losses in the previous iteration
+        lambdas_hat = [losses[i] / (self.last_losses[i] * self.temperature + EPS) for i in range(len(losses))]
+        lambdas_hat = tf.nn.softmax(lambdas_hat - tf.reduce_max(lambdas_hat)) * tf.cast(len(losses), dtype=tf.float32)
 
-          # compute new lambdas w.r.t. the losses in the first iteration
-          init_lambdas_hat = [losses[i] / (self.init_losses[i] * self.temperature + EPS) for i in range(len(losses))]
-          init_lambdas_hat = tf.nn.softmax(init_lambdas_hat - tf.reduce_max(init_lambdas_hat)) * tf.cast(len(losses), dtype=tf.float32)
+        # compute new lambdas w.r.t. the losses in the first iteration
+        init_lambdas_hat = [losses[i] / (self.init_losses[i] * self.temperature + EPS) for i in range(len(losses))]
+        init_lambdas_hat = tf.nn.softmax(init_lambdas_hat - tf.reduce_max(init_lambdas_hat)) * tf.cast(len(losses), dtype=tf.float32)
 
-          # use rho for deciding, whether a random lookback should be performed
-          new_lambdas = [(rho * alpha * self.lambdas[i] + (1 - rho) * alpha * init_lambdas_hat[i] + (1 - alpha) * lambdas_hat[i]) for i in range(len(losses))]
-          self.lambdas = new_lambdas
+        # use rho for deciding, whether a random lookback should be performed
+        new_lambdas = [(rho * alpha * self.lambdas[i] + (1 - rho) * alpha * init_lambdas_hat[i] + (1 - alpha) * lambdas_hat[i]) for i in range(len(losses))]
+        self.lambdas = new_lambdas
 
-          # store current losses in self.last_losses to be accessed in the next iteration
-          self.last_losses = losses
-          # in first iteration, store losses in self.init_losses to be accessed in next iterations
-          first_iteration = tf.cast(self.call_count <= self.iter1, dtype=tf.float32)
-          self.init_losses = [loss * first_iteration + var * (1 - first_iteration) for var, loss in zip(self.init_losses, losses)]
+        # store current losses in self.last_losses to be accessed in the next iteration
+        self.last_losses = losses
+        # in first iteration, store losses in self.init_losses to be accessed in next iterations
+        first_iteration = tf.cast(self.call_count <= self.iter1, dtype=tf.float32)
+        self.init_losses = [loss * first_iteration + var * (1 - first_iteration) for var, loss in zip(self.init_losses, losses)]
 
         # compute weighted loss
         loss = tf.reduce_sum([lam * loss for lam, loss in zip(self.lambdas, losses)])
 
-        tf.print(self.lambdas)
+        #tf.print(self.lambdas)
         self.call_count.assign_add(1)
       else:
         loss  = ( self.alpha[0]*uMse   + self.alpha[1]*vMse + self.alpha[2]*pMse \
